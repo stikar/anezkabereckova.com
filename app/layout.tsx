e4364f7cf9e687325f5b2207a713dbbac33bff75
Michal Landsman <landsman@studioart.cz>
2026-01-05 19:06:40 +0100

    fix: theme provider after update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { ThemeProvider } from '@/components/theme-provider'
+import { ThemeProvider } from 'next-themes'
 import './globals.css'
 
 export const metadata: Metadata = {
